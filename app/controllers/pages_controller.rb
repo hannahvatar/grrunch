47bf91db84fb9acd08278531a9f800bfb196099f
Raphael Blenessy-Menard <blenessyraphael@gmail.com>
2024-12-06 11:42:36 -0500

    Added functionnality to the search bar in the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   # before_action :authenticate_user!, only: [:home]
 
   def home
+    @products = []
+    if params[:query].present?
+      @products = Product.search_by_name_and_brand(params[:query])
+    end
   end
 
   def preferences
