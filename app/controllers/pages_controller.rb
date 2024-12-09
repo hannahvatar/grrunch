d38b0819e5b377a2412251f3b12f702690f32245
Anabelle Dubois <anabelle.dubois@icloud.com>
2024-12-09 17:42:12 -0500

    Lowest recorded div without carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
-  # before_action :authenticate_user!, only: [:home]
 
-  def home
-    @products = []
-    if params[:query].present?
-      @products = Product.search_by_name_and_brand(params[:query])
-    end
+def home
+  @products = Product.includes(:stores, :product_prices).order("RANDOM()").limit(3).all
+  if params[:query].present?
+    @products = Product.search_by_name_and_brand(params[:query])
   end
+end
 
   def preferences
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # config/environments/production.rb
+config.action_mailer.default_url_options = { host: 'grrunch.com' }
 end
