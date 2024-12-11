aa54bf1c30cf308eed7b173f4536194241cada48
Raphael Blenessy-Menard <blenessyraphael@gmail.com>
2024-12-11 10:52:51 -0500

    Updated the functionnalities of the data table.

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -4,4 +4,9 @@ class ProductPrice < ApplicationRecord
 
   validates :price, presence: true
   # validates :scraping_date, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_brand,
+    associated_against: {
+      product: [:name, :brand]
+}
 end
